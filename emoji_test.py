4a25db2b84914d665c13cd44a66084a50e5f9370
24fmifmhranat <24fmif.m.hranat@std.udu.edu.ua>
2025-10-07 13:54:33 +0300

    Update emoji_test.py

--- emoji_test.py
+++ emoji_test.py
@@ -1,4 +1,8 @@
 import random
 
 emotions = ["😄", "😂", "🤓", "😎", "🥳", "🤖"]
+
+print("Hello, world! Today I feel like", random.choice(emotions))
+
+emotions = ["😄", "😂", "🤓", "😎", "🥳", "🤖"]
 print("Hello, world! Today I feel like", random.choice(emotions))
