92e1b866642988d9605c5346e3506ac1743c3f15
24fmifmhranat <24fmif.m.hranat@std.udu.edu.ua>
2025-10-07 14:05:42 +0300

    Delete emoji_test.py

--- emoji_test.py
+++
@@ -1,8 +0,0 @@
-import random
-
-emotions = ["😄", "😂", "🤓", "😎", "🥳", "🤖"]
-
-print("Hello, world! Today I feel like", random.choice(emotions))
-
-emotions = ["😄", "😂", "🤓", "😎", "🥳", "🤖"]
-print("Hello, world! Today I feel like", random.choice(emotions))
