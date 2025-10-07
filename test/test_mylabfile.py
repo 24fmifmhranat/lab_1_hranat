a933f42c3a70d224991068a83f4cc93aec62d18b
24fmifmhranat <24fmif.m.hranat@std.udu.edu.ua>
2025-10-07 15:37:06 +0300

    Update test_mylabfile.py

--- test/test_mylabfile.py
+++ test/test_mylabfile.py
@@ -1,21 +1,21 @@
 import math
 
 def test_addition():
-    """Проверка сложения двух чисел"""
+    """Перевірка додавання двох чисел"""
     assert 3 + 5 == 8
     assert -2 + 7 == 5
 
 def test_factorial():
-    """Проверка вычисления факториала"""
+    """Підрахунок факторіалу"""
     assert math.factorial(5) == 120
     assert math.factorial(0) == 1
 
 def test_division():
-    """Проверка деления с округлением"""
+    """Ділення з округленням"""
     result = round(10 / 3, 2)
     assert result == 3.33
 
 def test_power():
-    """Проверка возведения в степень"""
+    """Піднесення до степеню"""
     assert pow(2, 3) == 8
     assert pow(5, 0) == 1
